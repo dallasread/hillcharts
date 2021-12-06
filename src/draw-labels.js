8274c5d51c75f350bd81ce2bcc55c49c0f32b870
Dallas Read <dallas@excitecreative.ca>
2021-12-05 21:16:13 -0400

    Basic label positions

--- src/draw-labels.js
+++ src/draw-labels.js
@@ -1,48 +1,67 @@
 const colors = ['#35c9f6', '#803bc7']
 
+const getCoordinates = (padding, width, height, value) => {
+  if (value === 0) {
+    return {
+      x: padding,
+      y: height - padding,
+      align: 'left'
+    }
+  } else if (value === 25) {
+    return {
+      x: width / 4,
+      y: height / 2,
+      align: 'center'
+    }
+  } else if (value === 50) {
+    return {
+      x: width / 2,
+      y: padding,
+      align: 'center'
+    }
+  } else if (value === 75) {
+    return {
+      x: width * 3 / 4,
+      y: height / 2,
+      align: 'center'
+    }
+  } else {
+    return {
+      x: 0,
+      y: 0,
+      align: 'right'
+    }
+  }
+}
+
+const addLabel = (ctx, padding, width, height, labelPadding, text, value, color) => {
+  const textHeight = height / 22
+  ctx.font = `bold ${textHeight}px`
+
+  const textWidth = ctx.measureText(text).width
+  const coords = getCoordinates(padding, width, height, value)
+
+  ctx.textAlign = coords.align
+  ctx.textBaseline = 'middle'
+
+  ctx.fillStyle = color
+  ctx.fillRect(coords.x - (textWidth / 2) - labelPadding, coords.y - (textHeight / 2) - labelPadding, textWidth + (labelPadding * 2), textHeight + (labelPadding * 2))
+
+  ctx.fillStyle = 'white'
+  ctx.fillText(text, coords.x, coords.y)
+}
+
 export default (ctx, labels, padding, width, height, labelPadding) => {
   let colorIndex = -1
   const chooseColor = () => {
     colorIndex++
-    if (colorIndex >= colors.length) colorIndex = -1
+    if (!colors[colorIndex]) colorIndex = 0
     return colors[colorIndex]
   }
 
+  ctx.globalAlpha = 1.0
+
   for (const label in labels) {
-    const value = labels[label]
-
-    ctx.globalAlpha = 1.0
-    const fontsize = height / 18
-    ctx.font = `bold ${fontsize}px Helvetica`
-    const textWidth = ctx.measureText(label).width
-    const textHeight = fontsize * 1.286
-
-    ctx.textAlign = 'center'
-    ctx.textBaseline = 'middle'
-
-    ctx.fillStyle = chooseColor()
-    ctx.fillRect(100 - (textWidth / 2) - labelPadding, 100 - (textHeight / 2) - labelPadding, textWidth + (labelPadding * 2), textHeight + (labelPadding * 2))
-
-    ctx.fillStyle = 'white'
-    ctx.fillText(label, 100, 100)
+    addLabel(ctx, padding, width, height, labelPadding, label, parseInt(labels[label]), chooseColor())
   }
-
-  //   const image = canvas.createCanvas(200, 200)
-  //   const ctx = image.getContext('2d')
-  //
-  //   ctx.font = '30px Times'
-  //   ctx.rotate(0.1)
-  //   ctx.fillText(q, 50, 100)
-  //
-  //   const text = ctx.measureText(q)
-  //   ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-  //   ctx.beginPath()
-  //   ctx.lineTo(50, 102)
-  //   ctx.lineTo(50 + text.width, 102)
-  //   ctx.stroke()
-  //
-  //   return image.toBuffer()
-
-  // #
-  // #803bc7
 }
