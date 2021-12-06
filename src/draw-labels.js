609bb0050ba349785d2d39771e3c6be5f9c19505
Dallas Read <dallas@excitecreative.ca>
2021-12-05 22:36:32 -0400

    Better placement

--- src/draw-labels.js
+++ src/draw-labels.js
@@ -1,42 +1,14 @@
 const colors = ['#FF7875', '#5ABBCA', '#C384C0', '#F7C164', '#777BB3']
 
-const getCoordinates = (padding, width, height, value) => {
+const getXOffset = (padding, width, height, value) => {
   if (value === 0) {
-    return {
-      x: padding,
-      y: height - padding,
-      align: 'left'
-    }
-  } else if (value === 25) {
-    return {
-      x: width / 4,
-      y: height / 2,
-      align: 'center'
-    }
-  } else if (value === 50) {
-    return {
-      x: width / 2,
-      y: padding,
-      align: 'center'
-    }
-  } else if (value === 75) {
-    return {
-      x: width * 3 / 4,
-      y: height / 2,
-      align: 'center'
-    }
+    return width / 30
   } else if (value === 100) {
-    return {
-      x: width,
-      y: height - padding,
-      align: 'right'
-    }
+    return width / -30
+  } else if (value === 50) {
+    return 0
   } else {
-    return {
-      x: 0,
-      y: 0,
-      align: 'right'
-    }
+    return ((value / 100) * width) * 0.02 * (value > 50 ? 1 : -1)
   }
 }
 
@@ -44,17 +16,29 @@ const addLabel = (ctx, padding, width, height, labelPadding, text, value, color)
   const textHeight = height / 18
   ctx.font = `bold ${textHeight}px Helvetica`
 
+  if (value > 90) {
+    value = 90
+  } else if (value < 10) {
+    value = 10
+  }
+
   const textWidth = ctx.measureText(text).width
-  const coords = getCoordinates(padding, width, height, value)
+  const coords = {
+    x: width * (value / 10) / 10,
+    y: Math.max((
+      (height - labelPadding) - ((height * ((value ^ 3) * 2) / 100))
+    ) * (value > 50 ? -1 : 1), padding),
+    xOffset: getXOffset(padding, width, height, value) || 0
+  }
 
   ctx.textAlign = coords.align
   ctx.textBaseline = 'middle'
 
   ctx.fillStyle = color
-  ctx.fillRect(coords.x - (textWidth / 2) - labelPadding, coords.y - (textHeight / 2) - labelPadding, textWidth + (labelPadding * 2), textHeight + (labelPadding * 2))
+  ctx.fillRect(coords.x - (textWidth / 2) - labelPadding + coords.xOffset, coords.y - (textHeight / 2) - labelPadding, textWidth + (labelPadding * 2), textHeight + (labelPadding * 2))
 
   ctx.fillStyle = 'white'
-  ctx.fillText(text, coords.x, coords.y)
+  ctx.fillText(text, coords.x + coords.xOffset, coords.y)
 }
 
 export default (ctx, labels, padding, width, height, labelPadding) => {
